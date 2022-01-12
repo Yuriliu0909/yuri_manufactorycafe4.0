f9a2e4a01bf8819ba205823647da0c774cbd4c3a
Yuriliu0909 <liuziyu99@gmail.com>
2022-01-13 01:13:36 +1300

    only add item one by one

--- cafe.js
+++ cafe.js
@@ -7,19 +7,37 @@ const app = express();
 const mongoose = require("mongoose");
 const _ = require("lodash");
 
+//set ejs templates link html&js
 app.set('view engine', 'ejs');
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+//access to public sources for ejs
 app.use(express.static("public"));
 
+//mongo atlas connection
 mongoose.connect("mongodb+srv://admin_yuri:123@cluster0.peek7.mongodb.net/coffeeDB?retryWrites=true&w=majority", {useNewUrlParser: true});
 
+//blow is tap pages
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/location", function(req, res){
+    res.render("location");
+});
+
+app.get("/comment", function(req, res){
+    res.render("comment");
+});
+
+//below is shopping methods
 const CoffeeSchema = new mongoose.Schema(
     {
         name: {
             type:String,
-            required: false
+            required:[true,"please check your name entry,no name specified!"]
         },
         price: {
             type:Number,
@@ -34,52 +52,33 @@ const CoffeeSchema = new mongoose.Schema(
 
 const CoffeeModel = mongoose.model('coffees', CoffeeSchema);
 
+const listSchema = {
+    name: String,
+    items: [CoffeeSchema]
+};
 
+const List = mongoose.model("List", CoffeeSchema);
+
+//show default stuff in coffeeDB at shop page
 app.get('/shop', async (req, res) => {
     await CoffeeModel.find({}, (err, result) => {
         if(err) {
             res.send(err);
         } else {
-            res.render("list", {listTitle: "Today", newListItems: result});
-            // res.send(result);
+            res.render("list", {listTitle: "Yummy Coffees", newListItems: result});
         }
     });
 })
 
-const coffeeSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,"please check your name entry,no name specified!"]
-    },
-    price:Number,
-    quantity:{
-        type:Number,
-        min:1,
-        max:100
-    },
-});
-
-const Coffee = mongoose.model("Coffee",coffeeSchema);
-
-
-const listSchema = {
-    name: String,
-    items: [coffeeSchema]
-};
-
-const List = mongoose.model("List", listSchema);
-
-
-
+//add new items into db
 app.post("/", async function(req, res){
     const itemName = req.body.newItem;
     const listName = req.body.list;
-
     const item = new CoffeeModel({
         name: itemName
     });
 
-    if (listName === "Today"){
+    if (listName === "Yummy Coffees"){
         await item.save();
         res.redirect("/shop");
     } else {
@@ -91,12 +90,15 @@ app.post("/", async function(req, res){
     }
 });
 
-app.post("/delete", function(req, res){
+app.post("/delete", async function(req, res){
     const checkedItemId = req.body.checkbox;
     const listName = req.body.listName;
+    const item = new CoffeeModel({
+        name: itemName
+    });
 
-    if (listName === "Today") {
-        Coffee.findByIdAndRemove(checkedItemId, function(err){
+    if (listName === "Yummy Coffees") {
+        await item.findByIdAndRemove(checkedItemId, function(err){
             if (!err) {
                 console.log("Successfully deleted checked item.");
                 res.redirect("/");
@@ -113,14 +115,8 @@ app.post("/delete", function(req, res){
 
 
 
-app.get("/location", function(req, res){
-    res.render("location");
-});
 
-app.get("/comment", function(req, res){
-    res.render("comment");
-});
-
+//below is post methods
 const postSchema = {
     title: String,
     content: String
@@ -128,13 +124,7 @@ const postSchema = {
 
 const Post = mongoose.model("Post", postSchema);
 
-app.get("/", function(req, res){
-    res.render("home");
-});
-
-
 app.get("/community", function(req, res){
-
     Post.find({}, function(err, posts){
         res.render("community", {
             posts: posts
@@ -147,7 +137,6 @@ app.post("/comment", function(req, res){
         title: req.body.postTitle,
         content: req.body.postBody
     });
-
     post.save(function(err){
         if (!err){
             res.redirect("/community");
@@ -165,10 +154,9 @@ app.get("/posts/:postId", function(req, res){
             content: post.content
         });
     });
-
 });
 
-
+//below is apache server
 app.listen(8080, function() {
     console.log("Server started on port 8080");
 });
