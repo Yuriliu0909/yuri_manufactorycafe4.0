f7b7f0129d09164a3da1719043fcfdf2f2db0fe7
Yuriliu0909 <liuziyu99@gmail.com>
2022-01-12 23:11:59 +1300

    fix the home page layout

--- cafe.js
+++ cafe.js
@@ -45,63 +45,6 @@ app.get('/shop', async (req, res) => {
     });
 })
 
-
-
-app.get("/location", function(req, res){
-    res.render("location");
-});
-
-app.get("/comment", function(req, res){
-    res.render("comment");
-});
-
-const postSchema = {
-    title: String,
-    content: String
-};
-
-const Post = mongoose.model("Post", postSchema);
-
-app.get("/", function(req, res){
-
-    Post.find({}, function(err, posts){
-        res.render("home", {
-            posts: posts
-        });
-    });
-});
-
-
-app.get("/compose", function(req, res){
-    res.render("compose");
-});
-
-app.post("/compose", function(req, res){
-    const post = new Post({
-        title: req.body.postTitle,
-        content: req.body.postBody
-    });
-
-    post.save(function(err){
-        if (!err){
-            res.redirect("/");
-        }
-    });
-});
-
-app.get("/posts/:postId", function(req, res){
-
-    const requestedPostId = req.params.postId;
-
-    Post.findOne({_id: requestedPostId}, function(err, post){
-        res.render("post", {
-            title: post.title,
-            content: post.content
-        });
-    });
-
-});
-
 const coffeeSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -117,24 +60,7 @@ const coffeeSchema = new mongoose.Schema({
 
 const Coffee = mongoose.model("Coffee",coffeeSchema);
 
-// const flatwhite = new Coffee({
-//     name:"flat white",
-//     price:5.5,
-//     quantity:30
-// });
-//
-// const espresso = new Coffee({
-//     name:"espresso",
-//     price:4,
-//     quantity:100
-// });
-//
-// const longblack = new Coffee({
-//     name:"long black",
-//     price:4.5,
-//     quantity:50
-// });
-//
+
 const listSchema = {
     name: String,
     items: [coffeeSchema]
@@ -142,43 +68,7 @@ const listSchema = {
 
 const List = mongoose.model("List", listSchema);
 
-// app.get("/shop", function(req, res) {
-    // CoffeeModel.find({}, function(err, foundItems){
-    //     if (foundItems.length === 0) {
-    //         Coffee.insertMany([flatwhite,espresso,longblack],function(err){
-    //             if(err){
-    //                 console.log(err);
-    //             }else{
-    //                 console.log("Successfully saved all the coffees to coffeeDB");
-    //             }
-    //         });
-    //         res.redirect("/");
-    //     } else {
-    //         res.render("list", {listTitle: "Today", newListItems: foundItems});
-    //     }
-    // });
-// });
 
-// app.get("/:customListName", function(req, res){
-//     const customListName = _.capitalize(req.params.customListName);
-//     List.findOne({name: customListName}, function(err, foundList){
-//         if (!err){
-//             if (!foundList){
-//                 //Create a new list
-//                 const list = new List({
-//                     name: customListName,
-//                     items: [flatwhite,espresso,longblack]
-//                 });
-//                 list.save();
-//                 res.redirect("/" + customListName);
-//             } else {
-//                 //Show an existing list
-//
-//                 res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
-//             }
-//         }
-//     });
-// });
 
 app.post("/", async function(req, res){
     const itemName = req.body.newItem;
@@ -218,6 +108,64 @@ app.post("/delete", function(req, res){
             }
         });
     }
+});
+
+
+
+app.get("/location", function(req, res){
+    res.render("location");
+});
+
+app.get("/comment", function(req, res){
+    res.render("comment");
+});
+
+
+const postSchema = {
+    title: String,
+    content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+    Post.find({}, function(err, posts){
+        res.render("home", {
+            posts: posts
+        });
+    });
+});
+
+
+app.get("/comment", function(req, res){
+    res.render("comment");
+});
+
+app.post("/comment", function(req, res){
+    const post = new Post({
+        title: req.body.postTitle,
+        content: req.body.postBody
+    });
+
+    post.save(function(err){
+        if (!err){
+            res.redirect("/");
+        }
+    });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+    const requestedPostId = req.params.postId;
+
+    Post.findOne({_id: requestedPostId}, function(err, post){
+        res.render("post", {
+            title: post.title,
+            content: post.content
+        });
+    });
+
 });
 
 
